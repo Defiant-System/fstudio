e97b49a3549588a7a6ea1cc015d9cc952732f1a8
Hakan Bilgin <hbi99@hotmail.com>
2024-02-26 11:10:16 +0100

    Update design.js

--- src/js/areas/design.js
+++ src/js/areas/design.js
@@ -13,13 +13,13 @@
 
 		this.data = {
 			draw: {
-				fill: "#db5728",
-				stroke: "#771607",
+				fill: "#11111122",
+				stroke: "#66666677",
 				strokeWidth: 1,
-				outline: true,
 				anchors: true,
+				handles: true,
 			},
-			fontSize: 300,
+			fontSize: 350,
 			view: {
 				dx: 300,
 				dy: 380,
@@ -78,13 +78,21 @@
 			// draw glyph path anchors + handles
 			for (let i=0, il=commands.length; i<il; i += 1) {
 				let cmd = commands[i];
-				if (cmd.x !== undefined) anchors.push({ x: cmd.x, y: -cmd.y });
-				if (cmd.x1 !== undefined) handles.push({ x: cmd.x1, y: -cmd.y1 });
-				if (cmd.x2 !== undefined) handles.push({ x: cmd.x2, y: -cmd.y2 });
+				if (cmd.x !== undefined) {
+					anchors.push({ x: cmd.x, y: -cmd.y });
+				}
+				if (cmd.x1 !== undefined) {
+					let anchor = anchors[anchors.length-2];
+					handles.push({ ox: anchor.x, oy: anchor.y, x: cmd.x1, y: -cmd.y1 });
+				}
+				if (cmd.x2 !== undefined) {
+					let anchor = anchors[anchors.length-1];
+					handles.push({ ox: anchor.x, oy: anchor.y, x: cmd.x2, y: -cmd.y2 });
+				}
 			}
 
-			this.anchors(ctx, anchors, Data.view.dx, Data.view.dy, scale);
-			// this.handles(ctx, handles, Data.view.dx, Data.view.dy, scale);
+			if (Data.draw.anchors) this.anchors(ctx, anchors, Data.view.dx, Data.view.dy, scale);
+			if (Data.draw.handles) this.handles(ctx, handles, Data.view.dx, Data.view.dy, scale);
 		},
 		path(ctx, path) {
 			var i, cmd, x1, y1, x2, y2;
@@ -122,11 +130,11 @@
 			}
 		},
 		anchors(ctx, l, x, y, scale) {
-			let size = 7,
+			let size = 6,
 				hS = size * .5;
 			ctx.fillStyle = "#fff";
-			ctx.strokeStyle = "#00a7fa";
-			for (let j=0; j<l.length; j+=1) {
+			ctx.strokeStyle = "#999";
+			for (let j=0, jl=l.length; j<jl; j+=1) {
 				let rx = x + (l[j].x * scale) - hS,
 					ry = y + (l[j].y * scale) - hS;
 				ctx.fillRect(rx, ry, size, size);
@@ -134,13 +142,24 @@
 			}
 		},
 		handles(ctx, l, x, y, scale) {
-			ctx.fillStyle = "red";
+			ctx.fillStyle = "#fff";
+			ctx.strokeStyle = "#999";
+
 			ctx.beginPath();
-			for (let j=0; j<l.length; j+=1) {
-				ctx.moveTo(x + (l[j].x * scale), y + (l[j].y * scale));
-				ctx.arc(x + (l[j].x * scale), y + (l[j].y * scale), 4, 0, Math.PI * 2, false);
+			for (let j=0, jl=l.length; j<jl; j+=1) {
+				ctx.moveTo(x + (l[j].ox * scale), y + (l[j].oy * scale));
+				ctx.lineTo(x + (l[j].x * scale), y + (l[j].y * scale));
 			}
 			ctx.closePath();
+			ctx.stroke();
+
+			ctx.beginPath();
+			for (let j=0, jl=l.length; j<jl; j+=1) {
+				ctx.moveTo(x + (l[j].x * scale), y + (l[j].y * scale));
+				ctx.arc(x + (l[j].x * scale), y + (l[j].y * scale), 3, 0, Math.PI * 2, false);
+			}
+			ctx.closePath();
+			ctx.stroke();
 			ctx.fill();
 		}
 	}
