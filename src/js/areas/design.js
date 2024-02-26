1ca39ce1f61d194ea40ffe475fda6eee8fc99039
Hakan Bilgin <hbi99@hotmail.com>
2024-02-26 13:47:43 +0100

    design view pan

--- src/js/areas/design.js
+++ src/js/areas/design.js
@@ -7,11 +7,17 @@
 		this.els = {
 			el: window.find(".view-design"),
 			cvs: window.find("canvas.glyph-editor"),
+			content: window.find("content"),
 		};
 		// get reference to canvas
 		this.els.ctx = this.els.cvs[0].getContext("2d");
 
+		// bind event handlers
+		this.els.cvs.on("mousedown", this.viewPan);
+
+		// view preferences
 		this.data = {
+			tool: "pan",
 			draw: {
 				fill: "#11111122",
 				stroke: "#66666677",
@@ -21,9 +27,9 @@
 			},
 			fontSize: 350,
 			view: {
-				dx: 300,
-				dy: 380,
-				dz: 1,
+				dX: 300,
+				dY: 380,
+				dZ: 1,
 			}
 		};
 	},
@@ -37,20 +43,17 @@
 		switch (event.type) {
 			case "window.resize":
 				el = Self.els.cvs.parent();
-				width = el.prop("offsetWidth");
-				height = el.prop("offsetHeight");
-				Self.els.cvs.attr({ width, height });
+				Self.data.cvsDim = {
+					width: el.prop("offsetWidth"),
+					height: el.prop("offsetHeight"),
+				};
+				Self.els.cvs.attr(Self.data.cvsDim);
 				break;
 			case "init-view":
-				el = Self.els.cvs.parent();
-				width = el.prop("offsetWidth");
-				height = el.prop("offsetHeight");
-				Self.els.cvs.attr({ width, height });
-				// ctx.translate(.5, .5);
-
+				if (!Self.data.cvsDim.width) Self.dispatch({ type: "window.resize" });
 				Self.data.glyph = Font.glyphs.get(37);
 
-				Self.draw.glyph();
+				Self.draw.glyph(Self);
 				break;
 			case "zoom-minus":
 			case "zoom-plus":
@@ -60,10 +63,9 @@
 		}
 	},
 	draw: {
-		glyph() {
-			let APP = glyphr,
-				Data = APP.design.data,
-				ctx = APP.design.els.ctx,
+		glyph(Self) {
+			let Data = Self.data,
+				ctx = Self.els.ctx,
 				glyph = Data.glyph,
 				scale = 1 / glyph.path.unitsPerEm * Data.fontSize,
 				commands = glyph.path.commands,
@@ -71,8 +73,12 @@
 				handles = [],
 				path;
 
+			// reset canvas
+			Self.els.cvs.attr(Self.data.cvsDim);
+			// ctx.translate(.5, .5);
+
 			// draw glyph base
-			path = glyph.getPath(Data.view.dx, Data.view.dy, Data.fontSize);
+			path = glyph.getPath(Data.view.dX, Data.view.dY, Data.fontSize);
 			path.fill = Data.draw.fill;
 			path.stroke = Data.draw.stroke;
 			path.strokeWidth = Data.draw.strokeWidth;
@@ -94,8 +100,8 @@
 				}
 			}
 
-			if (Data.draw.anchors) this.anchors(ctx, anchors, Data.view.dx, Data.view.dy, scale);
-			if (Data.draw.handles) this.handles(ctx, handles, Data.view.dx, Data.view.dy, scale);
+			if (Data.draw.anchors) this.anchors(ctx, anchors, Data.view.dX, Data.view.dY, scale);
+			if (Data.draw.handles) this.handles(ctx, handles, Data.view.dX, Data.view.dY, scale);
 		},
 		path(ctx, path) {
 			var i, cmd, x1, y1, x2, y2;
@@ -166,8 +172,47 @@
 			ctx.fill();
 		}
 	},
-	pan(event) {
-		let Self = glyphr,
+	viewPan(event) {
+		let Self = glyphr.design,
 			Drag = Self.drag;
+		switch(event.type) {
+			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+
+				let el = $(event.target),
+					doc = $(document),
+					offset = {
+						y: Self.data.view.dY,
+						x: Self.data.view.dX,
+					},
+					click = {
+						y: event.clientY,
+						x: event.clientX,
+					};
+
+				Self.drag = { el, doc, click, offset };
+
+				// cover app body
+				Self.els.content.addClass("cover hide-cursor");
+				// bind events
+				Self.drag.doc.on("mousemove mouseup", Self.viewPan);
+				break;
+			case "mousemove":
+				let dY = event.clientY - Drag.click.y + Drag.offset.y,
+					dX = event.clientX - Drag.click.x + Drag.offset.x;
+				
+				Self.data.view.dY = dY;
+				Self.data.view.dX = dX;
+
+				Self.draw.glyph(Self);
+				break;
+			case "mouseup":
+				// cover app body
+				Self.els.content.removeClass("cover hide-cursor");
+				// bind events
+				Drag.doc.off("mousemove mouseup", Self.viewPan);
+				break;
+		}
 	}
 }
