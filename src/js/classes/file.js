253186f138d8e8c0e1789872348a438c4af3a92a
Hakan Bilgin <hbi99@hotmail.com>
2024-02-27 13:32:51 +0100

    testing karaqu app cache

--- src/js/classes/file.js
+++ src/js/classes/file.js
@@ -6,14 +6,33 @@ class File {
 		// reference to loaded font object
 		this.font = OpenType.parse(fsFile.arrayBuffer);
 
-		console.log( this.font );
+		let cvs = $(`<canvas width="44" height="40" style="position: absolute; top: 0; left: 0;"></canvas>`)[0],
+			ctx = cvs.getContext("2d", { willReadFrequently: true }),
+			glyph = this.font.glyphs.get(1);
+
+		ctx.fillStyle = "#000";
+		glyph.draw(ctx, 15, 26, 25);
+
+		// window.cache.get("/app/ant/glyphr/cache/temp11.png").then(e => console.log( e ));
+		// window.cache.clear("temp11.png").then(e => console.log( e ));
+
+		cvs.toBlob(async blob => {
+			let test = await window.cache.set({ name: "temp11.png", blob });
+			console.log( `url('~/cache/temp11.png')` );
+		});
+
+		setTimeout(() => {
+			window.find(`.glyph-list .glyph[data-id="0x41"]`).css({ "--bg": `url('~/cache/temp11.png')` });
+		}, 500);
+		
+
 
 		let xParent = window.bluePrint.selectSingleNode(`//Data/Files`);
 		let xAttr = [],
 			xGroups = [];
 
 		xAttr.push(`name="${this.font.tables.name.fontFamily.en}"`);
-		xAttr.push(`style="${this.font.tables.name.fontSubfamily.en}"`);
+		xAttr.push(`style="${this.font.tables.name.fontSubfamily.en || "Regular"}"`);
 		xAttr.push(`glyphs="${this.font.numGlyphs}"`);
 		xAttr.push(`filename="${this.base}"`);
 		xAttr.push(`size="${+(fsFile.size / 1024).toFixed(1)} kB"`);
